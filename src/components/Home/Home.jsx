b3b882df4076c27f5f976a3ae71d8bab16695ca2
belek <bekakurmankulov1400@gmail.com>
2023-08-05 13:56:01 +0600

    add home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,14 +2,14 @@ import s from "./Home.module.css"
 const Home = () => {
     return (
         <div className={s.main}>
-            <div className={s.type__class}>
-                Vegetables
+            <div className={`${s.type__class} ${s.vegetables}`}>
+                <p className={s.title__type_class}>Vegetables</p>
             </div>
             <div className={s.type__class}>
-                Fruits
+                <p className={s.title__type_class}>Fruits</p>
             </div>
             <div className={s.type__class}>
-                Animals
+                <p className={s.title__type_class}>Animals</p>
             </div>
         </div>
     )
