cb55cab06dc28e9545b93c49d8bd1adad8db8e42
Anixii <kenjebekoverbol53@gmail.com>
2023-08-05 11:00:00 +0600

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import dog from './assets/22222222222222.png'
+import car from './assets/111111.png'
+function App() {
+  return (
+    <div className="main">
+      <img alt='' className={'imgage_dog'} src={dog}/>
+      <img alt='' className={'imgage_car'} src={car}/>
+    </div>
+  );
+}
+
+export default App;
