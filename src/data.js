551bb8f9a7f686ca366e2ed5d5ac7fb803f978ee
Anixii <kenjebekoverbol53@gmail.com>
2023-08-05 12:07:46 +0600

    .

---
+++ src/data.js
@@ -0,0 +1,12 @@
+import dogWhite from './assets/22222222222222.png' 
+const data = { 
+    animals: [
+        {type: 'Dog', action: 'Standing on paws',picture: dogWhite,color: 'White'},       
+        {type: 'Cat', action: 'Standing on paws',picture: dogWhite,color: 'White'},       
+        {type: 'LO', action: 'Standing on paws',picture: dogWhite,color: 'White'},       
+        {type: 'AS', action: 'Standing on paws',picture: dogWhite,color: 'White'},       
+        {type: 'Dogasd', action: 'Standing on paws',picture: dogWhite,color: 'White'},       
+        {type: 'asdDog', action: 'Standing on paws',picture: dogWhite,color: 'White'},       
+        {type: 'AAAADosadg', action: 'Standing on paws',picture: dogWhite,color: 'White'},       
+    ]
+}
